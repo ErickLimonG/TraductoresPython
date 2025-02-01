b23370ecd81ffa71688b9562822ae487f8723bdf
ErickLimonG <tolsericko@hotmail.com>
2025-01-31 19:43:43 -0600

    Add initial project structure and lexer implementation for Python analysis

---
+++ Actividad2/src/practica2/GeneradorFlexLexico.java
@@ -0,0 +1,39 @@
+/*ERICK ANTONIO LIMON GUEVARA*/
+/*PIEDRA FLORES AXEL JESUS*/
+/*LEONARDO JAVIER RODRIGUEZ MARTINEZ*/
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package practica2;
+
+import jflex.Main;
+
+/**
+ *
+ * @author Alumno
+ */
+public class GeneradorFlexLexico {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String rutaFlex="src/practica2/lexico.flex";
+        
+        
+            String[] jflexArgs = { rutaFlex };
+            try {
+               Main.generate(jflexArgs);
+               System.out.println("Lexer generado con exito");
+               }
+            
+            catch (Exception e)
+             {
+              System.err.println("Error al generar el lexico");
+              e.printStackTrace();
+              System.exit(1);
+             }
+    }   
+}
---
+++ Actividad2/src/practica2/sym.java
@@ -0,0 +1,18 @@
+/*ERICK ANTONIO LIMON GUEVARA*/
+/*PIEDRA FLORES AXEL JESUS*/
+/*LEONARDO JAVIER RODRIGUEZ MARTINEZ*/
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package practica2;
+
+/**
+ *
+ * @author axelp
+ */
+class sym {
+
+    static int EOF;
+    
+}
